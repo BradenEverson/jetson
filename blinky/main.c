4f459c4c801fdb2d744c75dcdcc25e4e1494d810
BradenEverson <bradeneverson@gmail.com>
2025-09-11 09:03:29 -0500

    Blink!

--- blinky/main.c
+++ blinky/main.c
@@ -5,11 +5,19 @@
 #include <jetgpio.h>
 
 int main() {
-    int32_t init = gpioInitialise();
-    if (init < 0) {
-        printf("Ut oh... oh no %d\n", init);
-        exit(init);
+    int32_t status = gpioInitialise();
+    if (status < 0) {
+        printf("Ut oh... oh no %d\n", status);
+        exit(status);
     }
 
     printf("GPIO Initialized :)\n");
+
+    status = gpioSetMode(8, JET_OUTPUT);
+    if (status < 0) {
+        printf("Setting pin to output failed with status code %d\n", status);
+        exit(status);
+    }
+
+    gpioWrite(8, 1);
 }
